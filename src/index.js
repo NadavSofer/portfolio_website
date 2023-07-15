8eeebc1a9710c29a150a259220b4d17af146e7d3
NadavSofer <nadavsof5@gmail.com>
2023-07-15 20:16:03 +0300

    added tab name

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = 'Nadav Sofer';
+
 root.render(
   <BrowserRouter>
     <React.StrictMode>
