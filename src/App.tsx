9cfed3d334654898d85c0bdf4c6dad92eb924fb6
Nadav <nadavsof5@gmail.com>
2025-04-23 19:05:47 +0300

    added basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import './App.css'
+import About from "./Components/About"
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import NavBar from "./Components/NavBar"
+import Projects from "./Components/Projects"
+import Tech from "./Components/Tech"
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-blue-500 text-4xl text-slate-300'>fish</h1>
+      <NavBar></NavBar>
+      <Header></Header>
+      <Tech></Tech>
+      <About></About>
+      <Projects></Projects>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Hi, I'm Nadav</h1>
+        <h2></h2>
+        <p></p>
+    </div>
+  )
+}
+
+export default Header
